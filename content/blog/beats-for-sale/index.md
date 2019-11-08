a86b1da08bc734b57c5f846dd2ed264c1bc128f3
PtrMark <piotr.markiewicz@ttms.pl>
2019-11-08 23:23:38 +0100

    gatsby london starter init

---
+++ content/blog/beats-for-sale/index.md
@@ -0,0 +1,6 @@
+---
+title: BEATS FOR SALE
+date: "2019-05-06"
+thumbnail: ./cassette.jpg
+description: What if everything you knew was turned to blue?
+---
---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Ortalio',
+  shortName: 'ortalio',
+  description: 'Music, Lyrics, Voice',
+  url: 'https://ortalio.pl', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Piotr Markiewicz',
+  twitter: 'PtrMarkiewicz'
+}
